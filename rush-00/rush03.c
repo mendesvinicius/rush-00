d4a948fa84f44adc28f4a3d080f8664b57cca41c
Marcio Vinicius Mendes Barbosa <mvmb@intranet.pitang.com>
2021-12-04 19:06:05 -0300

    refatorando codigo

--- rush-00/rush03.c
+++ rush-00/rush03.c
@@ -1,25 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 17:45:56 by marvin            #+#    #+#             */
+/*   Updated: 2021/12/04 17:45:56 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_putchar.c"
 #include <stdio.h>
 
-void rush(int x, int y) {
-  char Caracter[3] = {'A', 'B', 'C'};
-  int contadorLinha = 1;
-  int contadorX;
-  while (contadorLinha <= y) {
-    contadorX = 1;
-    while (contadorX <= x) { 
-        if (contadorX == 1 && (contadorLinha == 1 || contadorLinha == y)) {
-                ft_putchar(Caracter[0]);
-      } else if (contadorX == x && (contadorLinha == 1 || contadorLinha == y)) {
-        ft_putchar(Caracter[2]);
-      } else if (contadorX == 1 || contadorX == x || contadorLinha == 1 || contadorLinha == y) {
-        ft_putchar(Caracter[1]);
-      } else {
-        ft_putchar(' ');
-      }
-      ++contadorX;
+void rush(int x, int y)
+{
+    int height = 1;
+    int width;
+
+    while (height <= y)
+    {
+        width = 1;
+        while (width <= x)
+        {
+            if (width == 1 && (height == 1 || height == y))
+            {
+                ft_putchar('A');
+            }
+            else if (width == x && (height == 1 || height == y))
+            {
+                ft_putchar('C');
+            }
+            else if (width == 1 || width == x || height == 1 || height == y)
+            {
+                ft_putchar('B');
+            }
+            else
+            {
+                ft_putchar(' ');
+            }
+            ++width;
+        }
+        ft_putchar('\n');
+        ++height;
     }
-    ft_putchar('\n');
-    ++contadorLinha;
-  }
 }
