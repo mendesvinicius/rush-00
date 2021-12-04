fb660f29e11909f0928c698cd08d32ef93905ada
Carlos Felipe <carlos.felipe@hotmail.com.br>
2021-12-04 17:32:49 -0300

    reduzir linhas de dentro da funçao para ficar com menos de 25 linhas

--- rush-00/rush03.c
+++ rush-00/rush03.c
@@ -1,53 +1,25 @@
-#include <stdio.h>
 #include "ft_putchar.c"
+#include <stdio.h>
 
-void rush(int x, int y)
-{
-    char Caracter[3] = {'A', 'B', 'C'};
-
-    int contadorLinha = 1;
-    int contadorX;
-    int contadorY;
-
-    while (contadorLinha <= y)
-    {
-        if (contadorLinha != 1 && contadorLinha != y)
-        {   
-            contadorY = 1;
-            while (contadorY <= x)
-            {
-                if (contadorY == 1 || contadorY == x)
-                {
-                    ft_putchar(Caracter[1]);
-                }
-                else
-                {
-                    ft_putchar(' ');
-                }
-                ++contadorY;
-            }
-        }
-        else
-        {  
-            contadorX = 1;
-            while (contadorX <= x)
-            {
-                if (contadorX == 1)
-                {
-                    ft_putchar(Caracter[0]);
-                }
-                else if (contadorX == x)
-                {
-                    ft_putchar(Caracter[2]);
-                }
-                else
-                {
-                    ft_putchar(Caracter[1]);
-                }
-                ++contadorX;
-            }
-        }
-        ft_putchar('\n');
-        ++contadorLinha;
+void rush(int x, int y) {
+  char Caracter[3] = {'A', 'B', 'C'};
+  int contadorLinha = 1;
+  int contadorX;
+  while (contadorLinha <= y) {
+    contadorX = 1;
+    while (contadorX <= x) { 
+        if (contadorX == 1 && (contadorLinha == 1 || contadorLinha == y)) {
+                ft_putchar(Caracter[0]);
+      } else if (contadorX == x && (contadorLinha == 1 || contadorLinha == y)) {
+        ft_putchar(Caracter[2]);
+      } else if (contadorX == 1 || contadorX == x || contadorLinha == 1 || contadorLinha == y) {
+        ft_putchar(Caracter[1]);
+      } else {
+        ft_putchar(' ');
+      }
+      ++contadorX;
     }
+    ft_putchar('\n');
+    ++contadorLinha;
+  }
 }
